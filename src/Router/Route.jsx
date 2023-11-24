e5bdf9d9e752eabcb4cbb6c2b878c58ab2883210
nomayen <nomayen.ohin@gmail.com>
2023-11-24 18:08:03 +0600

    route create

---
+++ src/Router/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const myCreatedRoute = createBrowserRouter([
+    {
+        path:'/',
+        element:<h1>Hello</h1>
+    }
+])
+
+export default myCreatedRoute;
