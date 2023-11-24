a5aa413b87dc3f1797dacc7055386f52e2772b69
nomayen <nomayen.ohin@gmail.com>
2023-11-24 22:31:58 +0600

    navbar and navmenu added and login components

---
+++ src/Pages/Favorites/Favorites.jsx
@@ -0,0 +1,9 @@
+const Favorites = () => {
+    return (
+        <div>
+            <h1>favorites</h1>
+        </div>
+    );
+};
+
+export default Favorites;
--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -1,9 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+import Favorites from "../Pages/Favorites/Favorites";
+import Login from "../Pages/Login/Login";
 
 const myCreatedRoute = createBrowserRouter([
     {
         path:'/',
-        element:<h1>Hello</h1>
+        element:<MainLayout></MainLayout>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            },
+            {
+                path:'/favorites',
+                element:<Favorites></Favorites>
+            },
+            {
+                path:'/login',
+                element:<Login></Login>
+            }
+        ]
     }
 ])
 
