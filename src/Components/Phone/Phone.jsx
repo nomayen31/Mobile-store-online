f0b07f03669dadda1a33510fab1ca4122743476f
nomayen <nomayen.ohin@gmail.com>
2023-11-26 00:50:38 +0600

    rating react

--- src/Components/Phone/Phone.jsx
+++ src/Components/Phone/Phone.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Rating from 'react-rating';
 import { Link } from 'react-router-dom';
 
 const Phone = ({ phone }) => {
@@ -26,17 +27,26 @@ const Phone = ({ phone }) => {
                         </p>
                     </div>
                     <p className="block font-sans text-sm antialiased font-normal leading-normal text-gray-700 opacity-75">
-                        {rating}
+
+
+
+
+                        <Rating
+                            initialRating={rating}
+                            emptySymbol={<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 640 512" className=" fill-current  text-[#d97706] "><path d="M341.5 13.5C337.5 5.2 329.1 0 319.9 0s-17.6 5.2-21.6 13.5L229.7 154.8 76.5 177.5c-9 1.3-16.5 7.6-19.3 16.3s-.5 18.1 5.9 24.5L174.2 328.4 148 483.9c-1.5 9 2.2 18.1 9.7 23.5s17.3 6 25.3 1.7l137-73.2 137 73.2c8.1 4.3 17.9 3.7 25.3-1.7s11.2-14.5 9.7-23.5L465.6 328.4 576.8 218.2c6.5-6.4 8.7-15.9 5.9-24.5s-10.3-14.9-19.3-16.3L410.1 154.8 341.5 13.5zM320 384.7V79.1l52.5 108.1c3.5 7.1 10.2 12.1 18.1 13.3l118.3 17.5L423 303c-5.5 5.5-8.1 13.3-6.8 21l20.2 119.6L331.2 387.5c-3.5-1.9-7.4-2.8-11.2-2.8z"/></svg>}
+                            fullSymbol={<svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 576 512" className=" fill-current  text-[#d97706] "><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z" /></svg>}
+                            readonly
+                        />
                     </p>
                 </div>
                 <div className="p-6 pt-0">
                     <Link to={`/phones/${id}`}>
-                    <button
-      className="block bg-gray-300 w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-      type="button"
-    >
-      See Details
-    </button>
+                        <button
+                            className="block bg-gray-300 w-full select-none rounded-lg bg-blue-gray-900/10 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-blue-gray-900 transition-all hover:scale-105 focus:scale-105 focus:opacity-[0.85] active:scale-100 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                            type="button"
+                        >
+                            See Details
+                        </button>
                     </Link>
                 </div>
             </div>
--- src/Pages/Favorites/Favorites.jsx
+++ src/Pages/Favorites/Favorites.jsx
@@ -7,15 +7,21 @@ const Favorites = () => {
       const [favorites, setFavorites] = useState([]);
       const [noFound, setNoFound] = useState("");
       const [isShow, setIsShow] = useState(false);
+      const [ totalPrice , setTotalPrice ] = useState(0)
 
       useEffect(()=>{
         const favoriteItems = JSON.parse(localStorage.getItem('favorites'))
+
        if (favoriteItems) {
         setFavorites(favoriteItems)
+        const total = favoriteItems.reduce((previousValue, currentValue)=> previousValue + currentValue.price , 0)
+        setTotalPrice(total)
        }else{
         setNoFound('No Data Found 😂')
        }
     },[])
+
+
     console.log(favorites);
     const handleRemove = () =>{
         localStorage.clear()
@@ -27,7 +33,11 @@ const Favorites = () => {
         <div className="my-10">
             {noFound ? <p className="h-[60vh] flex justify-center items-center text-2xl font-bold">{noFound}</p> : <div>
                 {
-                    favorites.length > 0 &&  <button  onClick={handleRemove} className="py-2 px-4  bg-green-500 block mx-auto">Delete All Favorites</button>
+                    favorites.length > 0 && 
+                     <div>
+                        <button  onClick={handleRemove} className="py-2 px-4  bg-green-500 block mx-auto">Delete All Favorites</button>
+                        <h1 className="text-center">Total Price : {totalPrice}</h1>
+                     </div>
                 }
                 <div className="grid grid-cols-2 gap-5 my-10">
                 {
@@ -42,7 +52,9 @@ const Favorites = () => {
                 }
                 </div>
 
-                <button  onClick={()=>setIsShow(!isShow)} className="py-2 px-4 my-10 bg-green-500 block mx-auto">{isShow ? 'See Less' : 'see More'}</button>
+                {
+                    favorites.length > 4 && <button  onClick={()=>setIsShow(!isShow)} className="py-2 px-4 my-10 bg-green-500 block mx-auto">{isShow ? 'See Less' : 'see More'}</button>
+                }
 
 
 
